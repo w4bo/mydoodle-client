f4efc60473be48491d8d6b55ae2999880e7c5bfa
mfrancia <matteo.francia2@studio.unibo.it>
2022-08-02 10:25:34 +0200

    remove useless files

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,13 @@ import "highlight.js/styles/github.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faLink, faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { domain, clientId as client_id } from "../auth_config.json";
-
+// import { domain, clientId as client_id } from "../auth_config.json";
+import dotenv from "dotenv";
+dotenv.config();
+const domain = "" + process.env.VUE_APP_AUTH0_DOMAIN;
+const client_id = "" + process.env.VUE_APP_AUTH0_CLIENT_ID;
 const app = createApp(App);
-
 library.add(faLink, faUser, faPowerOff);
-
 app
   .use(hljs)
   .use(createRouter(app))
